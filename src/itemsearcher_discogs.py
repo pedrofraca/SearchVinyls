6eb2761d6809087aa3570e29cfb10bac5bf28605
Pedro Fraca <pedro.fraca@gmail.com>
2011-06-29 10:04:49 +0200

    Added License GPLv3

--- src/itemsearcher_discogs.py
+++ src/itemsearcher_discogs.py
@@ -1,3 +1,18 @@
+#   This file is part of SearchVinyls.
+#
+#    SearchVinyls is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 import httplib
 from pydoc import deque
 import urllib
@@ -6,18 +21,11 @@ import BeautifulSoup
 from BeautifulSoup import BeautifulSoup
 from item import Item
 
-          # For processing HTML
-
-
-# To change this template, choose Tools | Templates
-# http://www.discogs.com/sell/list?format=Vinyl&st=&q=los+planetas
-
 __author__="pfraca"
 __date__ ="$Jun 2, 2011 2:17:20 PM$"
 
 def create_headers():
     headers = {}
-    #headers['Content-Type'] = 'application/x-www-form-urlencoded'
     headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.71 Safari/534.24'
     return headers
 
